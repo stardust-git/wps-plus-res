41e4bed6866f2ed10c3c9b6c031b6bfebd28a1dc
songsong <2951709680@qq.com>
2024-10-19 23:29:45 +0800

    feat:新增wps相关二次封装方法

--- wps-plugins/hello-wps/main.js
+++ wps-plugins/hello-wps/main.js
@@ -18,48 +18,12 @@ function OnWPSWorkTabLoad(ribbonUI) {
   return true;
 }
 
-// 激活插件Tab
-function activeTab(tabId) {
-  wps.ribbonUI.ActivateTab(tabId);
-}
-
-/**
- * 不落地保存文件
- */
-function saveFile() {
-  console.log('saveFile');
-  // if (wps.confirm("当前环境已打开该文件，是否重新再打开一份？")) {
-  //   //如果用户选择再次打开，则用在线方式打开
-  //   // wps.WpsApplication().Documents.OpenFromUrl(OADocURL, "", "");
-  //   wps.Workbooks.OpenFromUrl('http://127.0.0.1:1024/upload/123.xlsx',false);
-  // }
-  // const token = wps.PluginStorage.getItem('token');
-  // var doc = wps.EtApplication().ActiveWorkbook;
-  // if (!doc) {
-  // 	alert('空文档不能保存！');
-  // 	return;
-  // }
-  // try {
-  // 	//调用不落地上传方法
-  // 	doc.SaveAsUrl(doc.Name, `http://127.0.0.1:1024/file/upload?token=123456&filename=test`, 'file');
-  // 	// precheckAfterSaveIsFileOnline(doc);
-  // } catch (err) {
-  // 	alert('上传文档失败！请检查系统上传参数及网络环境，重新上传。');
-  // }
-}
-
-/**
- * 打开对话面板
- */
-function showDialog() {
-  wps.ShowDialog('http://localhost:6789/wps-demo', '测试弹框');
-}
-
 /**
  * Action
  */
 function OnAction() {
-  showDialog();
+  // EtUtils.saveAsUrl();
+  ComUtils.showDialog();
 }
 
 /**
@@ -70,39 +34,30 @@ function GetImage() {
 }
 
 /**
- * sendToWeb
- */
-function sendToWeb(msg) {
-  wps.OAAssist.WebNotify(JSON.stringify(msg), true);
-}
-
-/**
  *  web页面调用WPS加载项的方法入口
  */
 function dispatcher(info) {
   if (!info) return;
 
-  var fileName = info['fileName'] + '.xlsx';
-  var existFile = wps.PluginStorage.getItem(fileName);
+  const fileName = info['fileName'] + '.xlsx';
+  const existFile = ComUtils.getStore(fileName);
   if (existFile) {
-    sendToWeb({status: 10, message: '该文档已经在WPS打开，请勿重复打开！'});
+    ComUtils.sendToWeb({status: 10, message: '该文档已经在WPS打开，请勿重复打开！'});
     return false;
   }
-
-  wps.EtApplication().Workbooks.OpenFromUrl(info.downloadPath);
+  EtUtils.openFromUrl(info.downloadPath);
   wps.EtApplication().UserName = info['userName'];// 设置当前文档对应的用户名
 
   //Office文档打开后，设置该文档属性：从服务端来的OA文档
   info.isOA = 1; //设置OA打开文档的标志
 
-  var infoJSON = JSON.stringify(info);
+  const infoJSON = JSON.stringify(info);
 
-  wps.PluginStorage.setItem(fileName, infoJSON);
-  wps.PluginStorage.setItem('token', info['token']);
+  ComUtils.setStore(fileName, infoJSON);
+  ComUtils.setStore('token', info['token']);
 
-  wps.ribbonUI.Invalidate(); // 刷新Ribbon自定义按钮的状态
-
-  setTimeout(() => activeTab('wpsAddinTab_local'), 1000); // 激活页面必须要页签显示出来，所以做1秒延迟
-  sendToWeb({status: 0, message: '文档打开成功！'});
+  ComUtils.refreshRibbon();
+  setTimeout(() => ComUtils.activeTab('HelloWps'), 1000); // 激活页面必需要页签显示出来，所以做1秒延迟
+  ComUtils.sendToWeb({status: 0, message: '文档打开成功！'});
   return true;
 }
---
+++ wps-plugins/hello-wps/util.js
@@ -0,0 +1,110 @@
+window.ComUtils = (function () {
+  /**
+   * 激活插件Tab
+   * @param {string} tabId ribbon中的tabId
+   */
+  function activeTab(tabId) {
+    wps.ribbonUI.ActivateTab(tabId);
+  }
+
+  /**
+   * 发送到Web中的消息
+   * @param {Object} msg 消息信息
+   * @param {number} msg.status 消息状态码
+   * @param {string} msg.message 消息内容
+   */
+  function sendToWeb(msg) {
+    wps.OAAssist.WebNotify(JSON.stringify(msg), true);
+  }
+
+  /**
+   *  刷新Ribbon按钮的状态
+   */
+  function refreshRibbon() {
+    wps.ribbonUI.Invalidate();
+  }
+
+  /**
+   * 打开对话面板
+   * @param {string} [url] webView 链接
+   * @param {string} [title]
+   * @param {number} [width]
+   * @param {number} [height]
+   * @param {boolean} [isModal]
+   */
+  function showDialog(url = 'http://localhost:6789/wps-demo', title, width, height, isModal) {
+    wps.ShowDialog(url, title, width, height, isModal);
+  }
+
+  /**
+   * 获取存储，可以在wps应用中共享
+   * @param {string} key
+   * @returns {string}
+   */
+  function getStore(key) {
+    return wps.PluginStorage.getItem(key);
+  }
+
+  /**
+   * 设置存储，可以在wps应用中共享
+   * @param {string} key
+   * @param {string | number | boolean} value
+   */
+  function setStore(key, value) {
+    wps.PluginStorage.setItem(key, value);
+  }
+
+  return {
+    activeTab,
+    sendToWeb,
+    refreshRibbon,
+    showDialog,
+    getStore,
+    setStore,
+  };
+})();
+
+window.EtUtils = (function () {
+  /**
+   * 从Url中打开
+   * @param {string} [url] 服务端的Url
+   * @param {string | Function} [success] 成功的回调，回调为注册在window中的方法名或函数
+   * @param {string} [fail] 失败的回调，回调为注册在window中的方法名或函数
+   */
+  function openFromUrl(url = 'http://127.0.0.1:1024/upload/123.xlsx', success, fail) {
+    wps.Workbooks.OpenFromUrl(url, success, fail);
+  }
+
+  /**
+   * 将当前文件另存为到其他服务端Url上
+   * @param {string} [url] 服务端的Url
+   * @param {string | Function} [success] 成功的回调，回调为注册在window中的方法名或函数
+   * @param {string} [fail] 失败的回调，回调为注册在window中的方法名或函数
+   */
+  function saveAsUrl(url = `http://127.0.0.1:1024/file/upload?token=123456&filename=test`, success, fail) {
+    const doc = wps.EtApplication().ActiveWorkbook;
+    if (!doc) {
+      alert('空文档不能保存！');
+      return;
+    }
+    doc.SaveAsUrl(doc.Name, `http://127.0.0.1:1024/file/upload?token=123456&filename=test`, 'file', success, fail);
+  }
+
+  return {
+    openFromUrl,
+    saveAsUrl,
+  };
+})();
+
+window.WpsUtils = (function () {
+
+  return {};
+})();
+
+window.WppUtils = (function () {
+
+  return {};
+})();
+
+
+
